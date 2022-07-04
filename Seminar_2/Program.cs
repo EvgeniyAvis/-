345e881eb42ad45fb19bb1321ec19b0692136388
Bezborodov Evgeniy <1303538@mail.ru>
2022-07-04 10:01:44 +0300

    Программа к задаче 10

---
+++ Seminar_2/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+using System;
+
+Console.Clear();
+
+Console.Write("Enter the number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if ((number / 10 > 0) && (number / 100 == 0)){
+    Console.WriteLine("Enter the  correct number!");
+}
+else {
+number = number / 10;
+number = number % 10;
+Console.WriteLine(number);
+}
